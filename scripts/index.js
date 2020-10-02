2f6350fd4f0ea5510251e9346329473200cf6d63
Roman SP <prs@m-cti.ru>
2020-10-02 13:17:23 +0300

    feat: Реализовать форму JS

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,43 @@
-console.log('halo')
-const buttonOpenPopup = document.querySelector(".profile__edit-button");
-const buttonClosePopup = document.querySelector(".popup__close-button");
-const popup = document.querySelector(".popup");
+
+let buttonOpenPopup = document.querySelector(".profile__edit-button");
+let buttonClosePopup = document.querySelector(".popup__close-button");
+let popup = document.querySelector(".popup");
+let formElement = document.querySelector(".popup__form");
+let profileTittle = document.querySelector(".profile__tittle");
+let profileSubtittle = document.querySelector(".profile__subtittle");
+let nameInput = formElement.querySelector(".popup__form-item_type_name");
+let jobInput = formElement.querySelector(".popup__form-item_type_about");
+
+console.log (profileTittle.textContent );
+console.log (profileSubtittle.textContent );
 
 function popupToggle() {
+  if (profileTittle.textContent !== nameInput.value || profileSubtittle.textContent !== jobInput.value) {
+    nameInput.value = profileTittle.textContent;
+    jobInput.value = profileSubtittle.textContent;  
+  }
   popup.classList.toggle("popup_is-opened")
 }
-
 buttonOpenPopup.addEventListener("click", popupToggle);
 buttonClosePopup.addEventListener("click", popupToggle);
-console.log('halo')
+
+function onClickPopupBackground (event) {
+  if (event.target !== event.currentTarget) {
+    return
+    
+  }
+  popupToggle()
+}
+
+popup.addEventListener('click', onClickPopupBackground);
+
+function formSubmitHandler (evt) {
+  evt.preventDefault();
+
+  profileTittle.textContent = nameInput.value;
+  profileSubtittle.textContent = jobInput.value;
+  
+  popupToggle()
+}
+
+formElement.addEventListener('submit', formSubmitHandler); 
