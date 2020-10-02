4b97ae6390984ddbde275353bcf29e43c71c6222
Roman SP <prs@m-cti.ru>
2020-10-02 15:18:35 +0300

    try: 1st

--- scripts/index.js
+++ scripts/index.js
@@ -8,35 +8,31 @@ let profileSubtittle = document.querySelector(".profile__subtittle");
 let nameInput = formElement.querySelector(".popup__form-item_type_name");
 let jobInput = formElement.querySelector(".popup__form-item_type_about");
 
-console.log (profileTittle.textContent );
-console.log (profileSubtittle.textContent );
-
 function popupToggle() {
   if (profileTittle.textContent !== nameInput.value || profileSubtittle.textContent !== jobInput.value) {
     nameInput.value = profileTittle.textContent;
-    jobInput.value = profileSubtittle.textContent;  
+    jobInput.value = profileSubtittle.textContent;
   }
   popup.classList.toggle("popup_is-opened")
 }
 buttonOpenPopup.addEventListener("click", popupToggle);
 buttonClosePopup.addEventListener("click", popupToggle);
 
-function onClickPopupBackground (event) {
+function onClickPopupBackground(event) {
   if (event.target !== event.currentTarget) {
     return
-    
   }
   popupToggle()
 }
 
 popup.addEventListener('click', onClickPopupBackground);
 
-function formSubmitHandler (evt) {
+function formSubmitHandler(evt) {
   evt.preventDefault();
 
   profileTittle.textContent = nameInput.value;
   profileSubtittle.textContent = jobInput.value;
-  
+
   popupToggle()
 }
 
